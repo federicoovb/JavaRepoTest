f3c8a586f668721ca23e434bb48b9c1436bab4c3
violaboros.federico <violaboros.federico@TAVE-PC31.OSDBSESTO.local>
2020-11-10 13:02:59 +0100

    add endless loop

---
+++ src/provagit/ProvaGit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagit;
+
+/**
+ *
+ * @author violaboros.federico
+ */
+public class ProvaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        long i=0;
+        long e=50000;
+        while(true){
+            System.out.println("ehi ciao! " + i*e);
+            i++;
+        }
+    }
+    
+}
